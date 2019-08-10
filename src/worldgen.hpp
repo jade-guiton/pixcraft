7bc8bd326906eff389ce3a354df51e416216205e
Luapix <luapixel@gmail.com>
2019-08-10 11:23:30 +0200

    Added explicit generation seeds.

--- src/worldgen.hpp
+++ src/worldgen.hpp
@@ -2,10 +2,13 @@
 
 #include <cstdint>
 #include <cmath>
+#include <ctime>
+#include <random>
 
 #include "blocks.hpp"
 #include "chunk.hpp"
 #include "OpenSimplexNoise.hpp"
+#include "wyhash.h"
 
 class WorldGenerator {
 public:
@@ -14,8 +17,12 @@ public:
 	void generateChunk(Chunk& chunk, int32_t chunkX, int32_t chunkZ);
 
 private:
+	uint64_t seed;
 	OpenSimplexNoise noiseGen;
 	
+	static uint64_t newSeed();
+	
 	uint8_t getTerrainHeight(int32_t x, int32_t z);
+	
 	void generateTree(Chunk& chunk, int32_t chunkX, int32_t chunkZ, int8_t relX, int8_t relY);
 };
