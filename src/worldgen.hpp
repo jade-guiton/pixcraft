5d28aaaf4b93274b86eb34cffb81226ea51e2924
Luapix <luapixel@gmail.com>
2019-08-10 19:23:58 +0200

    Added river beds.

--- src/worldgen.hpp
+++ src/worldgen.hpp
@@ -16,7 +16,9 @@ private:
 	uint64_t seed;
 	OpenSimplexNoise terrainHeightNoise;
 	
+	static const uint8_t WATER_LEVEL = 29;
+	
 	uint8_t getTerrainHeight(int32_t x, int32_t z);
 	
-	void generateTree(Chunk& chunk, int32_t chunkX, int32_t chunkZ, int8_t relX, int8_t relY);
+	void generateTree(Chunk& chunk, int32_t chunkX, int32_t chunkZ, int8_t rootX, int8_t rootZ);
 };
