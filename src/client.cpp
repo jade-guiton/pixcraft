7e5621c0f20aabf766bebb886533a80ebee19d5a
Luapix <luapixel@gmail.com>
2019-08-15 19:03:07 +0200

    Added basic MSAA.

--- src/client.cpp
+++ src/client.cpp
@@ -39,6 +39,7 @@ GameClient::GameClient()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	
 	window = glfwCreateWindow(START_WIDTH, START_HEIGHT, windowTitle, nullptr, nullptr);
 	if(window == nullptr)
@@ -52,6 +53,8 @@ GameClient::GameClient()
 	glfwSetFramebufferSizeCallback(window, windowResizeCallback);
 	windowResizeCallback(window, START_WIDTH, START_HEIGHT);
 	glfwSwapInterval(-1);
+	
+	glEnable(GL_MULTISAMPLE);
 	
 	cursorProgram = loadCursorProgram();
 	glGenVertexArrays(1, &cursorVAO);
