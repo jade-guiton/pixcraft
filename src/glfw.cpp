fe1a33e04ab1b76cd0d0e6430632dbb98393cc3d
Luapix <luapixel@gmail.com>
2019-08-15 20:31:40 +0200

    Added OpenGL forward compatibility.

--- src/glfw.cpp
+++ src/glfw.cpp
@@ -5,6 +5,6 @@
 void checkGlErrors(const char* opDesc) {
 	GLenum error;
 	while((error = glGetError()) != GL_NO_ERROR) {
-		std::cerr << "OpenGL error " << std::hex << error << std::dec << " during " << opDesc << std::endl << "!";
+		std::cerr << "OpenGL error " << std::hex << error << std::dec << " during " << opDesc << "!" << std::endl;
 	}
 }
