e12a66701e4125b52d224ddf482d7d6a3e3aa63a
Jade Guiton <guiton.jade@gmail.com>
2019-09-29 13:28:29 +0200

    Split game logic from client object

--- src/pixcraft/client/client.hpp
+++ src/pixcraft/client/client.hpp
@@ -1,83 +1,51 @@
 #pragma once
 
-#include <tuple>
+#include <memory>
 
 #include "glfw.hpp"
-#include "pixcraft/util/glm.hpp"
 
-#include "face_renderer.hpp"
-#include "chunk_renderer.hpp"
-#include "entity_renderer.hpp"
-#include "particle_renderer.hpp"
-#include "hotbar.hpp"
 #include "input.hpp"
 #include "text.hpp"
-#include "gui.hpp"
-
-#include "pixcraft/server/world_module.hpp"
-#include "pixcraft/server/world.hpp"
-#include "pixcraft/server/chunk.hpp"
 
 namespace PixCraft {
+	class GameClient;
+	
+	class GameState {
+	public:
+		GameState(GameClient& client);
+		
+		virtual void update(float dt) = 0;
+		virtual void render() = 0;
+		
+	protected:
+		GameClient& client;
+	};
+	
 	class GameClient {
 	public:
 		GameClient();
 		~GameClient();
 		
-		void mainLoop();
+		void run();
 		
 		InputManager& getInputManager();
 		TextRenderer& getTextRenderer();
 		
+		void getWindowSize(int* width, int* height);
+		int getFPS();
+		
 	private:
 		static const int START_WIDTH = 800;
 		static const int START_HEIGHT = 600;
-		static constexpr float SKY_COLOR[3] = {0.75f, 0.9f, 1.0f};
-		
-		static const int LOADS_PER_FRAME = 1;
-		
-		static constexpr float PLAYER_REACH = 5.0f;
 		
 		GLFWwindow* window;
-		
-		bool antialiasing;
-		bool showDebug;
-		bool paused;
-		int renderDist;
-		float fogStart, fogEnd;
-		
-		bool showCommandLine;
-		std::string commandBuffer;
-		
 		InputManager input;
-		World world;
-		Player* player;
-		
-		FaceRenderer faceRenderer;
-		ChunkRenderer chunkRenderer;
-		EntityRenderer entityRenderer;
-		ParticleRenderer particleRenderer;
-		Hotbar hotbar;
 		TextRenderer textRenderer;
 		
-		ShaderProgram cursorProgram;
-		IndexBuffer<glm::vec2> cursorBuffer;
-		ShaderProgram colorOverlayProgram;
-		VertexBuffer<glm::vec2> colorOverlayBuffer;
-		ShaderProgram blockOverlayProgram;
-		IndexBuffer<glm::vec3> blockOverlayBuffer;
-		
-		Button testButton;
+		std::unique_ptr<GameState> gameState;
 		
 		int frameNo;
 		int FPS;
-		
-		void setAntialiasing(bool enabled);
-		void setRenderDistance(int renderDist);
-		
-		void executeCommand();
-		void update(float dt);
-		void render();
 		
 		friend void windowResizeCallback(GLFWwindow* window, int width, int height);
 	};
---
+++ src/pixcraft/client/play_state.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "client.hpp"
+#include "pixcraft/util/glm.hpp"
+
+#include "face_renderer.hpp"
+#include "chunk_renderer.hpp"
+#include "entity_renderer.hpp"
+#include "particle_renderer.hpp"
+#include "hotbar.hpp"
+#include "gui.hpp"
+
+#include "pixcraft/server/world_module.hpp"
+#include "pixcraft/server/world.hpp"
+#include "pixcraft/server/chunk.hpp"
+
+namespace PixCraft {
+	class PlayState : public GameState {
+	public:
+		PlayState(GameClient& client);
+		
+		void update(float dt) override;
+		void render() override;
+		
+	private:
+		static constexpr float SKY_COLOR[3] = {0.75f, 0.9f, 1.0f};
+		static const int LOADS_PER_FRAME = 1;
+		static constexpr float PLAYER_REACH = 5.0f;
+		
+		bool antialiasing;
+		bool showDebug;
+		bool paused;
+		int renderDist;
+		float fogStart, fogEnd;
+		
+		bool showCommandLine;
+		std::string commandBuffer;
+		
+		World world;
+		Player* player;
+		
+		FaceRenderer faceRenderer;
+		ChunkRenderer chunkRenderer;
+		EntityRenderer entityRenderer;
+		ParticleRenderer particleRenderer;
+		Hotbar hotbar;
+		
+		ShaderProgram cursorProgram;
+		IndexBuffer<glm::vec2> cursorBuffer;
+		ShaderProgram colorOverlayProgram;
+		VertexBuffer<glm::vec2> colorOverlayBuffer;
+		ShaderProgram blockOverlayProgram;
+		IndexBuffer<glm::vec3> blockOverlayBuffer;
+		
+		Button testButton;
+		
+		void setAntialiasing(bool enabled);
+		void setRenderDistance(int renderDist);
+		void executeCommand();
+	};
+}
