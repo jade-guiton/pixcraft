jade-guiton/pixcraft

commit b12111f9d3d73a57ad2080956f77e107be83a1dd

Added fullscreen functionality.

src/pixcraft/client/input.cpp
@@ -7,7 +7,7 @@
 using namespace PixCraft;
 
 InputManager::InputManager()
-	: window(nullptr), _capturingMouse(false), oldMouseX(0), oldMouseY(0), _justPressed(), _justClicked {false, false},
+	: window(nullptr), _capturingMouse(false), oldMousePos(0, 0), _justPressed(), _justClicked {false, false},
 	  _justScrolled(0) { }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
@@ -57,8 +57,7 @@ void InputManager::capturingMouse(bool capturingMouse) {
 		int width, height;
 		glfwGetWindowSize(window, &width, &height);
 		glfwSetCursorPos(window, width/2, height/2);
-		oldMouseX = width/2;
-		oldMouseY = height/2;
+		oldMousePos = glm::ivec2(width/2, height/2);
 	}
 }
 
@@ -71,10 +70,9 @@ bool InputManager::justClicked(int button) {
 }
 
 glm::vec2 InputManager::getMouseMovement() {
-	double newMouseX, newMouseY;
-	glfwGetCursorPos(window, &newMouseX, &newMouseY);
-	glm::vec2 mvt(newMouseX - oldMouseX, newMouseY - oldMouseY);
-	oldMouseX = newMouseX; oldMouseY = newMouseY;
+	glm::ivec2 newMousePos = getMousePosition();
+	glm::vec2 mvt = newMousePos - oldMousePos;
+	oldMousePos = newMousePos;
 	return mouseSensitivity * mvt;
 }
 

src/pixcraft/client/client.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 
 #include "play_state.hpp"
 #include "menu_state.hpp"
@@ -24,7 +25,7 @@ void windowResizeCallback(GLFWwindow* window, int width, int height) {
 	client.getTextRenderer().setViewport(width, height);
 }
 
-GameClient::GameClient() : nextGameState(nullptr), frameNo(0), FPS(0.0) {
+GameClient::GameClient() : nextGameState(nullptr), frameNo(0), FPS(0.0), fullscreen(false) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -43,12 +44,12 @@ GameClient::GameClient() : nextGameState(nullptr), frameNo(0), FPS(0.0) {
 	
 	std::cout << "Using OpenGL version " << GLVersion.major << "." << GLVersion.minor << std::endl;
 	
+	input.init(window);
+	textRenderer.init();
+	
 	glfwSetFramebufferSizeCallback(window, windowResizeCallback);
 	windowResizeCallback(window, START_WIDTH, START_HEIGHT);
 	glfwSwapInterval(-1);
-	
-	textRenderer.init();
-	input.init(window);
 	
 	TextureManager::loadTextures();
 	BlockRegistry::defineBlocks();
@@ -62,6 +63,36 @@ GameClient::GameClient() : nextGameState(nullptr), frameNo(0), FPS(0.0) {
 GameClient::~GameClient() {
 	textRenderer.free();
 	glfwDestroyWindow(window);
+}
+
+GLFWmonitor* getCurrentMonitor(GLFWwindow *window) {
+	int wx, wy, ww, wh;
+	glfwGetWindowPos(window, &wx, &wy);
+	glfwGetWindowSize(window, &ww, &wh);
+	
+	int nmonitors;
+	GLFWmonitor** monitors = glfwGetMonitors(&nmonitors);
+	
+	GLFWmonitor* bestmonitor = nullptr;
+	int bestoverlap = 0;
+	
+	for(int i = 0; i < nmonitors; ++i) {
+		const GLFWvidmode* mode = glfwGetVideoMode(monitors[i]);
+		int mx, my, mw, mh;
+		glfwGetMonitorPos(monitors[i], &mx, &my);
+		mw = mode->width;
+		mh = mode->height;
+		
+		int overlap = std::max(0, std::min(wx + ww, mx + mw) - std::max(wx, mx))
+			* std::max(0, std::min(wy + wh, my + mh) - std::max(wy, my));
+		
+		if(bestoverlap < overlap) {
+			bestoverlap = overlap;
+			bestmonitor = monitors[i];
+		}
+	}
+
+	return bestmonitor;
 }
 
 void GameClient::run() {
@@ -81,10 +112,25 @@ void GameClient::run() {
 		
 		glfwPollEvents();
 		gameState->update(dt);
+		if(input.justPressed(GLFW_KEY_F)) {
+			fullscreen = !fullscreen;
+			if(fullscreen) {
+				glfwGetFramebufferSize(window, &windowedWidth, &windowedHeight);
+				GLFWmonitor* monitor = getCurrentMonitor(window);
+				const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+				glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, GLFW_DONT_CARE);
+			} else {
+				GLFWmonitor* monitor = glfwGetWindowMonitor(window);
+				const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+				glfwSetWindowMonitor(window, nullptr, (mode->width - windowedWidth)/2, (mode->height - windowedHeight)/2,
+					windowedWidth, windowedHeight, GLFW_DONT_CARE);
+			}
+		}
 		input.clearAll();
 		
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
+		
 		gameState->render(width, height);
 		
 		now = glfwGetTime();

src/pixcraft/client/client.hpp
@@ -51,6 +51,8 @@ namespace PixCraft {
 		
 		int frameNo;
 		int FPS;
+		bool fullscreen;
+		int windowedWidth, windowedHeight;
 		
 		friend void windowResizeCallback(GLFWwindow* window, int width, int height);
 	};

src/pixcraft/client/input.hpp
@@ -39,7 +39,7 @@ namespace PixCraft {
 		GLFWwindow* window;
 		
 		bool _capturingMouse;
-		double oldMouseX, oldMouseY;
+		glm::ivec2 oldMousePos;
 		
 		std::unordered_set<int> _justPressed;
 		bool _justClicked[2];

src/pixcraft/client/play_state.cpp
@@ -194,7 +194,7 @@ void PlayState::update(float dt) {
 		player->handleKeys(input.getMovementKeys(), dt);
 		
 		glm::vec2 mouseMvt = input.getMouseMovement();
-		player->rotate(glm::vec3(-mouseMvt.y, -mouseMvt.x, 0));
+		player->rotate(glm::vec3(mouseMvt.y, -mouseMvt.x, 0));
 		
 		int scroll = input.justScrolled();
 		if(scroll > 0) {

commit c4dac14b4cb802ef67dd5e010499552d6db060a6

Fixed sudden mouse movement bug on unpausing (introduced in b12111f9)

src/pixcraft/client/input.cpp
@@ -57,7 +57,7 @@ void InputManager::capturingMouse(bool capturingMouse) {
 		int width, height;
 		glfwGetWindowSize(window, &width, &height);
 		glfwSetCursorPos(window, width/2, height/2);
-		oldMousePos = glm::ivec2(width/2, height/2);
+		oldMousePos = glm::ivec2(0, 0);
 	}
 }
 